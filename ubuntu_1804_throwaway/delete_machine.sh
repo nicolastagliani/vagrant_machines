8239d4b499c1e87d7a9181777eda6b762b5ae1fa
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2019-12-14 22:33:08 +0100

    Moved files in subfolder

--- ubuntu_1804_throwaway/delete_machine.sh
+++ ubuntu_1804_throwaway/delete_machine.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Entering folder ${DIR}"
+pushd ${DIR}
 MACHINE_NAME=ubuntu-1804-lts-throwaway 
 echo "Powering off machine ${MACHINE_NAME}"
 VBoxManage controlvm ${MACHINE_NAME} poweroff
 echo "Deleting machine ${MACHINE_NAME}"
 VBoxManage unregistervm ${MACHINE_NAME} --delete
 echo "Deleting local vagrant files"
-rm -rf .vagrant
+rm -vrf ./.vagrant
+popd
--- ubuntu_1804_throwaway/provision_script.sh
+++ ubuntu_1804_throwaway/provision_script.sh
@@ -3,24 +3,19 @@ apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
 #apt-get -o "Dpkg::Options::=--force-confold" upgrade -y
 apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y
-apt-get autoremove -y
-apt-get autoclean -y
 
-echo "Hello, I'm the provision script"
-echo "I'm running in $(uname) from $(whoami)"
+echo "Installing the rest of the packages"
+apt-get install -y  git git-lfs build-essential clang clang-format ccache python-pip ninja-build valgrind
+
+# Nice to have packages
+# Autocompletion in terminal for git
+apt-get install -y bash-completion clang-6.0 clang-tidy-6.0
+
 echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker vagrant
 rm get-docker.sh
-
-echo "Installing the rest of the packages"
-apt-get install -y build-essentials  git git-lfs build-essential clang clang-format ccache python-pip ninja-build valgrind
-# Nice to have packages
-# Autocompletion in terminal for git
-apt-get install -y bash-completion clang-6.0 clang-tidy-6.0
-
-pip install conan
 
 apt-get autoremove -y
 apt-get autoclean -y
