242246735d53bf36350cdaedf0133ff9c8875f77
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2020-07-04 22:19:27 +0200

    Added vagrant dev machine with Ubuntu 20.04 LTS

--- ubuntu_2004_dev_machine/provision_script.sh
+++ ubuntu_2004_dev_machine/provision_script.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get  -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" upgrade -q -y
 
 echo "Installing the rest of the packages"
-apt-get install -y  git git-lfs build-essential clang clang-format ccache python3-pip ninja-build valgrind cmake
+apt-get install -y net-tools git git-lfs build-essential clang clang-format ccache python3-pip ninja-build valgrind cmake
 
 # Nice to have packages
 # Autocompletion in terminal for git
