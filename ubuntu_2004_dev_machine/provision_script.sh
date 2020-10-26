e61743772286bb3b62cfce4ca492410851428052
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2020-10-26 20:21:57 +0100

    Added required packages for running opengl dev applications

--- ubuntu_2004_dev_machine/provision_script.sh
+++ ubuntu_2004_dev_machine/provision_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+
 # Upgrade and autoremove the packages with no interaction
 apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
@@ -5,6 +7,8 @@ apt-get  -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confol
 
 echo "Installing the rest of the packages"
 apt-get install -y net-tools git git-lfs build-essential clang clang-format ccache python3-pip ninja-build valgrind cmake
+echo "Installing opengl-dev packages"
+apt-get install -y libgl-dev pkg-config xorg-dev libx11-xcb-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev xkb-data  libglu1-mesa-dev
 
 # Nice to have packages
 # Autocompletion in terminal for git
